f7e80da5a0a79f2ab2d3f42cdfac608ae7d36bbc
Laurens Ruijtenberg <laurens.ruijtenberg@outlook.com>
2026-04-10 19:01:14 +0200

    cleanup

--- dist-electron/preload.js
+++
@@ -1,9 +0,0 @@
-let electron = require("electron");
-//#region electron/preload.ts
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-	extractDom: (webviewId) => electron.ipcRenderer.invoke("extract-dom", webviewId),
-	platform: process.platform
-});
-//#endregion
-
-//# sourceMappingURL=preload.js.map
